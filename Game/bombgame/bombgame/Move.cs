ccefb1638ff9c8920ce51a7d6debdbd37cfb9294
yumi0507 <128886113+yumi0507@users.noreply.github.com>
2023-05-01 01:23:56 +0800

    Add files via upload

---
+++ Game/bombgame/bombgame/Move.cs
@@ -0,0 +1,59 @@
+﻿using bombgame.Player;
+using bombgame.Map;
+using bombgame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bombgame.Move
+{
+    
+    private void Form1_KeyDown(object sender, KeyEventArgs e)
+    {
+        player pr;
+        map map1;
+
+        while (true)
+        {            
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                if (pr.pos_y < 5)
+                    pr.pos_y++;
+            }
+
+            else if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                if (pr.pos_y > 0)
+                    pr.pos_y--;
+            }
+
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                if (pr.pos_x < 5)
+                    pr.pos_x++;
+            }
+
+            else if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                if (pr.pos_x > 0)
+                    pr.pos_x--;
+            }
+
+            else if (e.KeyCode == Keys.Space)
+            {
+                if (pr.bomb_hold > 0)
+                {
+                    pr.bomb_hold--;
+                    map1.map_state[pr.pos_y, pr.pos_x] = 1;
+                }
+
+            }
+
+            if (timeleft! > 0)
+                break;
+        }
+    }
+}
---
+++ Game/bombgame/bombgame/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bombgame.Player
+{
+    public class player
+    {
+        public int id;
+        public string name;
+        public bool is_alive = true;
+
+        public int pos_x;
+        public int pos_y;
+        public bool valid_pos;
+
+        public int bomb_hold = 4;
+
+        public int Pos_x
+        {
+            set
+            {
+                if (id % 2 == 1) { pos_x = 0; }
+                if (id % 2 == 0) { pos_x = 5; }
+            }
+        }
+        public int Pos_y
+        {
+            set
+            {
+                if (id / 3 == 0) { pos_y = 0; }
+                if (id / 3 == 1) { pos_y = 5; }
+            }
+        }
+
+        public bool Valid_pos
+        {
+            get
+            {
+                if (pos_x <= 5 && pos_x >= 0 && pos_y <= 5 && pos_y >= 0)
+                { return true; }
+                return false;
+            }
+        }
+
+    }
+
+    
+}
